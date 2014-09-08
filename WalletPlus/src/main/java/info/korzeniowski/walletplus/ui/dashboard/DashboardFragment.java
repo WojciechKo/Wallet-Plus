824309914ac9ef29f1e89ecebbe7bcfe590b24ad
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2014-09-08 22:38:37 +0200

    Reorganize ui.dashboard package

--- WalletPlus/src/main/java/info/korzeniowski/walletplus/ui/dashboard/DashboardFragment.java
+++ WalletPlus/src/main/java/info/korzeniowski/walletplus/ui/dashboard/DashboardFragment.java
@@ -30,8 +30,7 @@ public class DashboardFragment extends Fragment {
     @InjectView(R.id.totalAmount)
     TextView totalAmount;
 
-    @Inject
-    @Named("local")
+    @Inject @Named("local")
     WalletService localWalletService;
 
     @Override
